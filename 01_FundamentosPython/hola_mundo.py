3886e2ea75f5f1542928dcd83fdf9a104c2b9719
Erick <erickrodriguez12ero@gmail.com>
2026-07-17 22:40:03 -0600

    feat: update hola_mundo.py

--- 01_FundamentosPython/hola_mundo.py
+++ 01_FundamentosPython/hola_mundo.py
@@ -1,2 +1,5 @@
 print("Hola mundo")
 #Esto me servira para ver lo que estoy configurando 
+print("Bienvenido al proyecto Alpha")
+print("Voy a comvertirme en un desarrollador de software.")
+#Dudas o comentarios despues de las 9:30 
