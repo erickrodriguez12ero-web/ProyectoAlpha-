6d635f4b5c884a49d5250fd560c6174a663fb191
Erick <erickrodriguez12ero@gmail.com>
2026-07-20 15:53:47 -0600

    feat: update variables.py

---
+++ Variables.py
@@ -0,0 +1,12 @@
+Nombre = "Marcus"
+Edad = 0 
+Estatura = 1.32
+es_estudiante = False
+carrera = "ing informatica"
+
+print("Nombre:", Nombre)
+print("La edad es:", Edad)
+print("Tu estatura es:", Estatura)
+print("¿Es estudiante?", es_estudiante)
+print("Su carrera es:", carrera)
+
